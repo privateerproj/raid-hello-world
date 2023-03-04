0e3de4e657c0c240e341165f9fe57665b9822885
Eddie Knight <knight@linux.com>
2023-03-04 07:58:20 -0600

    Removed old internal/ content

--- internal/summary/summary.go
+++
@@ -1,8 +0,0 @@
-package summary
-
-import (
-	audit "github.com/privateerproj/privateer-sdk/audit"
-)
-
-// State should be set in the pack's runtime via audit.NewSummaryState
-var State audit.SummaryState
